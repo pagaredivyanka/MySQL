cb9d9ab7fd5239d113ad8842c889c6528d318302
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-04-05 03:12:31 +0530

    Update query02.sql

--- query02.sql
+++ query02.sql
@@ -30,8 +30,8 @@ from orders
 where order_date >= '2019-01-01';
 
 -- Ex. 2 from the order_items table, get the items 
---       for order #6
--- 		   where the total price is greater than 30
+-- for order #6
+-- where the total price is greater than 30
 select * 
 from order_items
 where order_id = 6 and unit_price * quantity > 30; 
