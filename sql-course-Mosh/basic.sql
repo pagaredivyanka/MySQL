d66fc83194a4a34f77a4216ed8e4fd23a5f0a0f0
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-08-27 01:34:42 +0530

    Update basic.sql

--- sql-course-Mosh/basic.sql
+++ sql-course-Mosh/basic.sql
@@ -1,12 +1,20 @@
 create database shop;
 use shop;
-create table customer (id int primary key, first_name varchar(80), last_name varchar(80), City varchar(80), product varchar(300));
-select * from customer;
+create table customer (
+  id int primary key, 
+  first_name varchar(80), 
+  last_name varchar(80), 
+  City varchar(80), 
+  product varchar(300)
+);
+
 insert into customer values(101, 'AAA', 'BBB', 'Nashik', 'book');
 insert into customer values(102, 'CCC', 'DDD', 'adgau', 'pen');
 insert into customer values(103, 'EEE', 'FFF', 'pune', 'bag');
 insert into customer values(104, 'GGG', 'HHH', 'Nashik', 'book');
 insert into customer values(105, 'XXX', 'ZZZ', 'nagpur', null);
+
+
 select * from customer;
 
 
